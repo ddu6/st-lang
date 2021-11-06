6b2b3a06974a99b524b89d649c7f2dfd59f587f3
DDU <ddu6@protonmail.com>
2021-11-06 16:59:13 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as ston from 'ston'
 import * as stdn from 'stdn'
 import { IdType, extractIdsWithTag, extractIdsWithIndex, extractOrbitsWithTag } from './extract'
 import { URL } from 'url'
-const stViewVersion='0.2.35'
+const stViewVersion='0.2.36'
 const stylePatch=`html:not([data-color-scheme=light])>body.vscode-dark{
     --color-text: #cccccc;
     --color-light: #8f8f8f;
