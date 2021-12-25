f1d5cbd43a049d3abe75af30d039de2b6cabebe3
DDU <ddu6@protonmail.com>
2021-12-26 04:06:55 +0800

    update

--- out/extension.js
+++ out/extension.js
@@ -15,7 +15,7 @@ const katex_1 = require("./katex");
 const ston = require("ston");
 const stdn = require("stdn");
 const extract_1 = require("./extract");
-const stViewVersion = '0.8.1';
+const stViewVersion = '0.8.2';
 const stylePatch = `html:not([data-color-scheme=light])>body.vscode-dark{
     --color-text: #cccccc;
     --color-light: #8f8f8f;
