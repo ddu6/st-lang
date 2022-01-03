754a5f5848ff76203731d64f65684a6f3300e18d
DDU <ddu6@protonmail.com>
2022-01-03 19:22:01 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@ import * as stdn from 'stdn'
 import * as vscode from 'vscode'
 import {cmds} from './katex'
 import {IdType, extractIdsWithTag, extractIdsWithIndex, extractOrbitsWithTag} from './extract'
-const stViewVersion = '0.19.2'
+const stViewVersion = '0.20.0'
 const stylePatch = `html:not([data-color-scheme=light])>body.vscode-dark {
     --color-text: #cccccc;
     --color-light: #8f8f8f;
