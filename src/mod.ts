7648947e0ee32e4e5fb9808105b3ed0c8eb80b84
DDU <ddu6@protonmail.com>
2022-12-19 22:25:27 +0800

    update

--- src/mod.ts
+++ src/mod.ts
@@ -6,7 +6,7 @@ import * as vscode from 'vscode'
 import {cmds} from './katex'
 import {extractIds, extractOrbits, IdType} from './extract'
 const stuiVersion = '0.15.12'
-const stViewVersion = '0.31.16'
+const stViewVersion = '0.31.17'
 const css = `html:not([data-color-scheme=light])>body.vscode-dark {
     --color-text: rgb(204 204 204);
     --color-light: rgb(110 110 110);
